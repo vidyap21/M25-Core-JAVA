48b172464c9ba1fe536f783e3c5f847a81eccce2
Admin <Admin@DESKTOP-LU4P75A>
2022-09-29 09:37:42 +0530

    First Commit

---
+++ DAY2 -IntroductionToJava/src/org/tnsindia/datatype/DemoOnDatatype.java
@@ -0,0 +1,12 @@
+package org.tnsindia.datatype;
+
+public class DemoOnDatatype {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				//byte xyz=128
+				/*Byte range is till 127 so 128 is out of range	then
+				 we have to go ahead with short/int or long*/
+		long xyz=128;
+		System.out.println(xyz);
+	}
+}
