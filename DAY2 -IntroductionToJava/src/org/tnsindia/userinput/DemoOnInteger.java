f2d31eb81e241d7f3df831cee5576e951aa4bc80
Admin <Admin@DESKTOP-LU4P75A>
2022-09-29 11:29:32 +0530

    First Commit

---
+++ DAY2 -IntroductionToJava/src/org/tnsindia/userinput/DemoOnInteger.java
@@ -0,0 +1,17 @@
+package org.tnsindia.userinput;
+
+import java.util.Scanner;
+
+public class DemoOnInteger {
+
+	public static void main(String[] args) {
+	
+	//For user input value	
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("Enter The Value Of X:  ");
+		 int x=s.nextInt();
+		 System.out.println("The Result Is:  ");
+		 System.out.println(x);
+		 s.close();
+	}
+}
