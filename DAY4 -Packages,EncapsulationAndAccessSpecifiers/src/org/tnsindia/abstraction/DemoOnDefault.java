dca7269b6747d47fda0888c395ce0bed3e867580
Admin <Admin@DESKTOP-LU4P75A>
2022-09-30 15:07:05 +0530

    First Commit

---
+++ DAY4 -Packages,EncapsulationAndAccessSpecifiers/src/org/tnsindia/abstraction/DemoOnDefault.java
@@ -0,0 +1,24 @@
+package org.tnsindia.abstraction;
+
+class C
+{
+	
+	String VehicleType;          //default variable
+	
+}
+
+public class DemoOnDefault {
+
+	public static void main(String[] args) {
+		C c=new C();
+		
+		/*If any data members is default then it can access only inside the same package,
+		 * it can not be access outside the another package
+		 */
+		
+		c.VehicleType="Train";
+		System.out.println("Vehicle Type is: "+c.VehicleType);
+
+	}
+
+}
