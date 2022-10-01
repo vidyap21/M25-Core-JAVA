6aad5adef69c78f1520ce824d47e7ca8a4f5c448
Admin <Admin@DESKTOP-LU4P75A>
2022-10-01 19:39:23 +0530

    First Commit

---
+++ ASSIGNMENT - 1/src/com/tns/Jumping/Break.java
@@ -0,0 +1,15 @@
+package com.tns.Jumping;
+
+public class Break {
+	
+		  public static void main(String[] args) {
+		    int i = 0;
+		    while (i < 10) {
+		      System.out.println(i);
+		      i++;
+		      if (i == 7) {
+		        break;
+		      }
+		    }  
+		  }
+		}
---
+++ ASSIGNMENT - 1/src/com/tns/Jumping/Return.java
@@ -0,0 +1,13 @@
+package com.tns.Jumping;
+
+public class Return {
+	  static int myMethod(int x) {
+		    return 5 + x;
+		  }
+
+		  public static void main(String[] args) {
+		    System.out.println(myMethod(3));
+		  }
+		}
+
+
